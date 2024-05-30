cf5232c5f72810adfe4c1bdf78306098a8797445
Douglas Moraes <douglasgm89@live.com>
2024-05-30 14:07:23 -0300

    Hero section update

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './hero.css'
+import markersShowcase from '../assets/Markers_showcase.png'
 
 const hero = () => {
     return (
-        <div className='hero'>
-            <div className="hero-lef">
-                <h2></h2>
+        <div className='sp__hero section__padding'>
+            <div className="sp__hero-left">
+                <h2>SEJA BEM VINDO!</h2>
+                <h3>CONHEÇA NOSSAS COLEÇÕES!</h3>
+                <button type='button'>Útimas coleções</button>
             </div>
-            <div className="hero-right">
+            <div className="sp__hero-right">
+                <div className='sp__hero-right_text'>
+                    <p>Marcadores de Páginas para todos os gostos</p>
+                </div>
+                <img src={markersShowcase} />
 
             </div>
         </div>
